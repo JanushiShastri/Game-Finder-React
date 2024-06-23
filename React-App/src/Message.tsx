f9d16fe2668a384fcb56c3fdce78fd26ed1cec13
JanushiShastri <shastrijanushi@gmail.com>
2024-06-23 12:29:36 -0600

    added dynamic list group using hooks

---
+++ React-App/src/Message.tsx
@@ -0,0 +1,10 @@
+function Message(){
+    //jsx: javascript component
+    const name = "Janushi";
+    if (name)
+
+        return <h1>Hello {name}</h1>
+
+    return <h1>Hello World!</h1>
+}
+export default Message;
